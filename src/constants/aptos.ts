825283bae26c176f0b0a3deb7457240c3941bd23
31Vector31 <lutaiyura@gmail.com>
2023-11-06 23:44:06 +0200

    added aptos and token list constants

---
+++ src/constants/aptos.ts
@@ -0,0 +1,4 @@
+export const DEVNET_NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+export const TESTNET_NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
+export const SWAP_ADDRESS = "0x90fdf0b1ef78d8dc098e1e7cd3b6fe1f084c808484bc243a1da2a24e7ef06096";
