a76774959f49d2a8f21be09ecaa94358c66144f5
31Vector31 <lutaiyura@gmail.com>
2023-11-06 23:44:29 +0200

    added sundry utils

---
+++ src/utils/sundry.ts
@@ -0,0 +1,17 @@
+export const calculateRate = (amount: string, conversionRate: number) => {
+    return Number(amount) * conversionRate;
+};
+
+export const numberWithCommas = (number: number, decimal?: number) => {
+    if (!decimal) decimal = 8; // default number of decimals, 8
+
+    if (number == 0) return 0.0;
+
+    return Number(number.toFixed(decimal)).toLocaleString("en", {
+        minimumFractionDigits: decimal,
+    });
+};
+
+export const formatBalance = (balance: number, decimals: number) => {
+    return balance / 10 ** decimals;
+};
