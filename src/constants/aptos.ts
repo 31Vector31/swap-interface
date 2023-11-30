b9ca9179cf28a0f5cd7d6477cc4fb41f5c3a62b3
31Vector31 <lutaiyura@gmail.com>
2023-11-30 22:05:47 +0200

    fix aptos constants

--- src/constants/aptos.ts
+++ src/constants/aptos.ts
@@ -2,7 +2,7 @@ export const DEVNET_NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
 export const TESTNET_NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
 export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
 export const SWAP_ADDRESS = "0x90fdf0b1ef78d8dc098e1e7cd3b6fe1f084c808484bc243a1da2a24e7ef06096";
-export const SWAP_ADDRESS2 = "0x4ee0b37ec67bb47e8d5acf469c5282b93a78145b2ba268782bd7b1e73d411d7c";
+export const SWAP_ADDRESS2 = "0x4d054856f6afb8e25bb387a42f1e72c8b0deb999ab98b8ea4c8adf46273f8d63";
 export const APT_TOKEN_ADDRESS = "0x1::aptos_coin::AptosCoin";
 
 export const networks = ["Mainnet", "Testnet"];
